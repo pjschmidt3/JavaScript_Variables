e3cde233ea6888cdf71050af15a36d808f2998a8
Phillip Schmidt <pjschmidt3@outlook.com>
2021-12-06 14:08:30 -0600

    fix incorrect exercise

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName = 'Phillip';
+const lastName =- 'Schmidt';
+const age = 29;
 
 // Exercise 2
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
 // Exercise 3
+const city = 'Covington, LA';
+const pastime = 'coding';
+let myStory = `Hi, my name is ${fullName}. I currently live in ${city}. My favorite pastime is ${pastime}.`;
+
+console.log(myStory);
